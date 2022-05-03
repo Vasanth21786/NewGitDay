7622e6f6ed90d445cc110d21abf7cd26fa24af82
vasanth21786 <vasanth4156@gmail.com>
2022-05-03 11:33:55 +0530

    few lines of code added

---
+++ src/org/sample/Details.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Details {
+	private void data() {
+		System.out.println("print");
+
+	}
+	public static void main(String[] args) {
+		Details a=new Details();
+		a.data();
+	}
+
+}
